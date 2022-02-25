32e323e874983046cbcc40f492aa02209c3ed59c
abhaynit <87191075+abhaynit@users.noreply.github.com>
2022-02-25 22:14:55 +0530

    FHJ

--- bhaiya/settings.py
+++ bhaiya/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@2-@9ci#_9qfed!wou7*x3*$d5c8n2(tcj!-0tcdpakc4-xrf3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','bhaiyabirthdaygift.herokuapp.com']
 
