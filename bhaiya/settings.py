74a82448c76b14d9a7f5395ca7bbc473add45562
abhaynit <87191075+abhaynit@users.noreply.github.com>
2022-02-25 20:31:25 +0530

    hft

--- bhaiya/settings.py
+++ bhaiya/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (BASE_DIR/'media')
+#STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
